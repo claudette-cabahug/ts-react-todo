4ca34e6375cb88f8166bdf33f5451b9cf7801c84
Rodney Cabahug <rodney.cabahug@gmail.com>
2021-11-07 01:26:09 +1300

    refactor: simplify database connection string
    Used one environment variable for the database connection URL.

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,7 @@ const app: Express = express();
 app.use(cors);
 app.use(routes);
 
-const uri : string =
-    `mongodb+srv://
-    ${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}
-    @${process.env.MONGO_SERVER}/${process.env.MONGO_DB}
-    ?retryWrites=true&w=majority`;
-
-mongoose.connect(uri)
+mongoose.connect(`${process.env.MONGO_URL}`)
     .then(() => {
         const PORT: string | number = process.env.PORT || 4000;
         app.listen(PORT, () => {
